ebc38172ad24d109107524a156cb06216c098739
bola-sankar <23q71a42a8@aietta.ac.in>
2026-02-08 11:40:20 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
   plugins: [react()],
   base: "/you-are-my-heart-lilly/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+})
